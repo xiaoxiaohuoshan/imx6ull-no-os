7fbc46bc64523de5f11dc8859212cd7fb916b443
xiao.li <xiao.li@aispeech.com>
2020-02-28 04:00:04 -0800

    init project.001/008/016 can work, but not beautiful!

---
+++ 001_led/main.c
@@ -0,0 +1,29 @@
+
+#include "led.h"
+
+void delay(volatile unsigned int d)
+{
+	while(d--);
+}
+
+
+int  main()
+{
+	led_init();
+
+	while(1)
+	{
+		led_ctl(1);
+		delay(100000);
+		led_ctl(0);
+		delay(100000);
+		led_ctl(1);
+		delay(100000);
+		led_ctl(0);
+		delay(100000);
+	}
+					
+	return 0;
+}
+
+
---
+++ 8_UART串口编程/005_myprintf_test/main.c
@@ -0,0 +1,9 @@
+#include "my_printf.h"
+
+int  main()
+{
+	my_printf_test();			
+	return 0;
+}
+
+
